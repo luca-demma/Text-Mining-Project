a4d1458072d795de8794daa6339bb3ac6b32bf66
luca <Luca Demma>
2022-05-21 12:17:29 +0200

    ner improved

--- ner.py
+++ ner.py
@@ -11,14 +11,27 @@ data = json.load(file)
 print('READING DATA FROM FILE COMPLETED !')
 
 
+def ner_tree_to_list(tree):
+	ner_list = []
+	for chunk in tree:
+		if hasattr(chunk, 'label'):
+			ner_list.append({
+				"entity": "".join(c[0] for c in chunk), # used join to convert to string
+				"label": chunk.label()
+			})
+	return ner_list
+
+
 for news in data:
 	news['title_tokens_raw'] = word_tokenize(news['title'])
 	news['title_pos_tagged'] = pos_tag(news['title_tokens_raw'])
-	news['title_ner'] = nltk.ne_chunk(news['title_pos_tagged'])
+	ner_tree_title = nltk.ne_chunk(news['title_pos_tagged'])
+	news['title_ner'] = ner_tree_to_list(ner_tree_title)
 
 	news['description_tokens_raw'] = word_tokenize(news['description'])
 	news['description_pos_tagged'] = pos_tag(news['description_tokens_raw'])
-	news['description_ner'] = nltk.ne_chunk(news['description_pos_tagged'])
+	ner_tree_description = nltk.ne_chunk(news['description_pos_tagged'])
+	news['description_ner'] = ner_tree_to_list(ner_tree_description)
 	print(news['description'])
 	print(news['description_ner'])
 	print('///////////////////')
