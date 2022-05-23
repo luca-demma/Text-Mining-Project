a0a535ea48d67063990c28695d5fffd43abe36cc
luca <Luca Demma>
2022-05-23 15:13:22 +0200

    trying to make ner faster

--- ner.py
+++ ner.py
@@ -23,18 +23,23 @@ def ner_tree_to_list(tree):
 
 
 for news in tqdm(data):
-	news['title_tokens_raw'] = word_tokenize(news['title'])
-	news['title_pos_tagged'] = pos_tag(news['title_tokens_raw'])
-	ner_tree_title = nltk.ne_chunk(news['title_pos_tagged'])
-	news['title_ner'] = ner_tree_to_list(ner_tree_title)
+	title_tokens_raw = word_tokenize(news['title'])
+	title_pos_tagged = pos_tag(title_tokens_raw)
+	ner_tree_title = nltk.ne_chunk(title_pos_tagged)
+	title_ner = ner_tree_to_list(ner_tree_title)
 
-	news['description_tokens_raw'] = word_tokenize(news['description'])
-	news['description_pos_tagged'] = pos_tag(news['description_tokens_raw'])
-	ner_tree_description = nltk.ne_chunk(news['description_pos_tagged'])
-	news['description_ner'] = ner_tree_to_list(ner_tree_description)
+	news['title_tokens_raw'] = title_tokens_raw
+	news['title_pos_tagged'] = title_pos_tagged
+	news['title_ner'] = title_ner
+
+	description_tokens_raw = word_tokenize(news['description'])
+	description_pos_tagged = pos_tag(description_tokens_raw)
+	ner_tree_description = nltk.ne_chunk(description_pos_tagged)
+	description_ner = ner_tree_to_list(ner_tree_description)
+
+	news['description_tokens_raw'] = description_tokens_raw
+	news['description_pos_tagged'] = title_pos_tagged
+	news['description_ner'] = description_ner
 
 
 write_json_to_file(data, "ner.json")
-
-
-
